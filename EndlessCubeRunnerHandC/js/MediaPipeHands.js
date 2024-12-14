c6ca10bb6725867e74017fc4ab885ac9f3977cb7
aroncal02 <154700667+ronmaccms@users.noreply.github.com>
2024-12-14 13:50:43 -0500

    tests

--- EndlessCubeRunnerHandC/js/MediaPipeHands.js
+++ EndlessCubeRunnerHandC/js/MediaPipeHands.js
@@ -1,33 +1,30 @@
 export class MediaPipeHands {
-    constructor(videoElement, onResultsCallback) {
-      const hands = new Hands({
-        locateFile: (file) => {
-          return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-        },
-      });
-      hands.setOptions({
-        maxNumHands: 1,
-        modelComplexity: 1,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5,
-      });
-      hands.onResults(onResultsCallback);
-  
-      this.camera = new Camera(videoElement, {
-        async onFrame() {
-          await hands.send({ image: videoElement });
-        },
-        width: this.isMobile() ? 720 : 1280,
-        height: this.isMobile() ? 1280 : 720,
-      });
-    }
-  
-    isMobile() {
-      return window.innerWidth < window.innerHeight;
-    }
-  
-    start() {
-      if (this.camera) this.camera.start();
-    }
+  constructor(videoElement, onResultsCallback) {
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+
+    hands.setOptions({
+      maxNumHands: 1,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+
+    hands.onResults(onResultsCallback);
+
+    this.camera = new Camera(videoElement, {
+      async onFrame() {
+        await hands.send({ image: videoElement });
+      },
+      width: window.innerWidth < window.innerHeight ? 720 : 1280,
+      height: window.innerWidth < window.innerHeight ? 1280 : 720,
+    });
   }
-  
+
+  start() {
+    if (this.camera) this.camera.start();
+  }
+}
